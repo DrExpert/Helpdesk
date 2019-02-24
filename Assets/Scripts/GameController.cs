0194ee87e3b97a0218ef1f2e77c1710c5be20f00
mszym <marcel.szymanski@tensquaregames.com>
2019-02-24 10:58:53 +0100

    fixy do LVL 1 :)

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -13,9 +13,11 @@ public class GameController : MonoBehaviour
     private HandController hand;
 
     [SerializeField]
-    private Collider2D[] flatThings;
+    private List<Collider2D> flatThings;
     [SerializeField]
-    private ThingController[] allThings;
+    private List<ThingController> allThings;
+    [SerializeField]
+    private Transform thingsOnTableObject;
     [SerializeField]
     private Collider2D tableCollider, trashCollider;
     [SerializeField]
@@ -29,7 +31,11 @@ public class GameController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        ThingController[] things = GetComponentsInChildren<ThingController>();
+        foreach(ThingController thing in things){
+            allThings.Add(thing);
+            if(!thing.high)flatThings.Add(thing.gameObject.GetComponent<Collider2D>());
+        }
     }
 
     // Update is called once per frame
@@ -79,17 +85,17 @@ public class GameController : MonoBehaviour
 
     public float IshallGetHeight(Collider2D collider){
         int iter = 0;
-        float pos = 0;
+        float pos = 0.1f;
         foreach(Collider2D coll in flatThings){
             // Vector3 zeroHeight = new Vector3(collider.gameObject.transform.position.x, collider.gameObject.transform.position.y, coll.gameObject.transform.position.z);
             Bounds bounds = new Bounds(new Vector3(collider.bounds.center.x,collider.bounds.center.y,coll.gameObject.transform.position.z),collider.bounds.size);
-            if (coll != null && coll.bounds.Intersects(bounds) && coll.transform.position.z >= collider.transform.position.z ){
+            if (coll != null && coll.bounds.Intersects(bounds) ){//&& coll.transform.position.z >= collider.transform.position.z ){
                 iter++;
-                pos  = coll.transform.position.z + 0.1f;
+                if(coll.transform.position.z <= pos)pos  = coll.transform.position.z - 0.2f;
             } 
             //iter=collider.IsTouching(coll)?iter+1:iter;
         }
         Debug.Log(iter);
-        return iter;
+        return pos;
     } 
 }
